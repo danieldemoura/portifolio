f9890eb277661242f7e7cccf3aac8bab34420618
daniel de moura <nazuyamikima@gmail.com>
2025-04-09 15:03:29 -0300

    chore: configurado alias com @ no tsconfig e no vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
 
 export function cn(...inputs: ClassValue[]) {
