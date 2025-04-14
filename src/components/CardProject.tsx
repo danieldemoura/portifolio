88b629f49f3c6d6d11293881af123710f26b5881
daniel de moura <nazuyamikima@gmail.com>
2025-04-14 17:00:12 -0300

    fix: exibir botão no mobile sem depender do hover no elemento article

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,5 +1,3 @@
-import { GithubSVG } from "./svgs/GithubSVG";
-
 type Image = {
   img: string;
   title: string;
@@ -14,54 +12,45 @@ type CardProjectProps = {
 
 export function CardProject({ title, photo, icons, url }: CardProjectProps) {
   return (
-    <article
-      style={{ backgroundImage: `url(${photo})` }}
-      className="group/cardp relative overflow-hidden rounded-[2rem] border-2 border-sky-950 bg-size-[75%_70%] bg-center bg-no-repeat"
-    >
-      <div className="h-full space-y-4 bg-[rgba(170,170,170,0.1)] py-5 backdrop-blur-xl">
-        <header className="font-display w-full text-center italic lg:text-2xl">
+    <article className="h-full w-fit space-y-4 overflow-hidden rounded-[2rem] border-2 border-sky-950 bg-[rgba(170,170,170,0.1)] pb-6 backdrop-blur-xl">
+      <header className="flex w-full items-center justify-between bg-gray-950 px-6 py-3 text-center transition duration-300 lg:text-2xl">
+        <span className="font-display max-w-37 truncate italic peer-hover:text-black">
           {title}
-        </header>
-        <figure className="flex h-full w-full flex-col items-center gap-4 lg:gap-8">
-          <img
-            className="w-64 rounded-lg border-2 border-sky-900 shadow-xl shadow-black/50 lg:w-87.5 lg:shadow-black/70"
-            src={photo}
-            alt=""
-          />
-          <figcaption>
-            <p className="sr-only">
-              Esse projeto foi desenvolvido utilizando as seguintes tecnologias
-            </p>
-            <ul className="flex justify-center gap-[1.125rem]">
-              {icons.map((icon) => {
-                return (
-                  <li key={icon.title}>
-                    <img
-                      className="w-8 drop-shadow-2xl drop-shadow-black/60 lg:w-14"
-                      src={icon.img}
-                      alt=""
-                    />
-                    <span className="sr-only">{icon.title}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </figcaption>
-        </figure>
-      </div>
-
-      <div className="absolute top-0 grid h-full w-full -translate-x-315 place-items-center rounded-[2rem] bg-zinc-900/50 backdrop-blur-lg transition duration-800 group-hover/cardp:translate-x-0">
+        </span>
         <a
+          className="font-body peer rounded-lg border border-yellow-700 bg-yellow-400 px-7 text-lg font-semibold text-black transition hover:bg-yellow-700 hover:text-white"
           href={url}
           target="_blank"
-          className="group/deploy grid place-items-center text-lg drop-shadow-2xl drop-shadow-zinc-900 lg:text-2xl"
         >
-          <GithubSVG className="size-20 group-hover/deploy:text-yellow-500" />
-          <span className="font-display group-hover/deploy:text-yellow-500">
-            Acessar Projeto
-          </span>
+          Deploy
         </a>
-      </div>
+      </header>
+      <figure className="flex h-full w-full flex-col gap-4 px-6 lg:gap-8">
+        <img
+          className="w-64 rounded-lg shadow-xl shadow-black/50 lg:w-87.5 lg:shadow-black/70"
+          src={photo}
+          alt=""
+        />
+        <figcaption>
+          <p className="sr-only">
+            Esse projeto foi desenvolvido utilizando as seguintes tecnologias
+          </p>
+          <ul className="flex gap-[1.125rem]">
+            {icons.map((icon) => {
+              return (
+                <li key={icon.title}>
+                  <img
+                    className="size-8 drop-shadow-2xl drop-shadow-black/60 lg:size-12 lg:w-14"
+                    src={icon.img}
+                    alt=""
+                  />
+                  <span className="sr-only">{icon.title}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </figcaption>
+      </figure>
     </article>
   );
 }
